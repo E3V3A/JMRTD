c5a4b95c7d4700168745dbb73fe5843a1a72ddb7
martijno <martijno@32d68b02-0bac-436f-a684-189869639a7e>
2006-07-03 15:26:17 +0000

    - Small changes

--- passporthostapi/src/sos/mrtd/sample/PassportGUI.java
+++ passporthostapi/src/sos/mrtd/sample/PassportGUI.java
@@ -1,7 +1,7 @@
 /*
- * JMRTD - A Java package for accessing machine readable travel documents.
+ * JMRTD - A Java API for accessing machine readable travel documents.
  *
- * Copyright (C) 2006  ICIS, Radboud University
+ * Copyright (C) 2006  SoS group, Radboud University
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
