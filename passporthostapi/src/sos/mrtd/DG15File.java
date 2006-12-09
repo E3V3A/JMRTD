145dd80c3bd312858e184e7e14db84e5f6c826fb
martijno <martijno@32d68b02-0bac-436f-a684-189869639a7e>
2006-12-09 11:50:26 +0000

    - Added class for DG 15

---
+++ passporthostapi/src/sos/mrtd/DG15File.java
@@ -0,0 +1,72 @@
+/*
+ * JMRTD - A Java API for accessing machine readable travel documents.
+ *
+ * Copyright (C) 2006  SoS group, ICIS, Radboud University
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * $Id: $
+ */
+
+package sos.mrtd;
+
+import java.io.IOException;
+import java.security.PublicKey;
+
+import sos.smartcards.BERTLVObject;
+
+/**
+ * File structure for datagroup 15.
+ * Datagroup 15 contains the public key used in AA.
+ * 
+ * @author Cees-Bart Breunesse (ceesb@cs.ru.nl)
+ * @author Martijn Oostdijk (martijno@cs.ru.nl)
+ * 
+ * @version $Revision: $
+ */
+public class DG15File extends PassportFile
+{
+   private PublicKey publicKey;
+
+   /**
+    * Constructs a new file.
+    * 
+    * @param publicKey the key to store in this file
+    */
+   public DG15File(PublicKey publicKey) {
+      this.publicKey = publicKey;
+   }
+
+   public byte[] getEncoded() {
+      try {
+         BERTLVObject ef010F =
+            new BERTLVObject(PassportASN1Service.EF_DG15_TAG,
+                  publicKey.getEncoded());
+         return ef010F.getEncoded();
+      } catch (IOException e) {
+         e.printStackTrace();
+         return null;
+      }
+   }
+
+   /**
+    * Gets the public key stored in this file.
+    * 
+    * @return the public key
+    */
+   public PublicKey getPublicKey() {
+      return publicKey;
+   }
+}
